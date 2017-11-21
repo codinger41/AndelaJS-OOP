feaaa247d09219b91876f002c8a5cafb6abcd149
leksyib <leksyib14@gmail.com>
2017-11-21 09:57:52 +0100

    write test to know if Cat, Dog and Shark are subclasses of Animal

---
+++ tests/test.js
@@ -0,0 +1,25 @@
+import chai from 'chai';
+import { Dog, Cat, Animal, Shark } from "../src/index.js";
+
+import {expect} from 'chai';
+
+ describe("Cat",() =>{
+        it("Should be a subclass of Animal",() => {
+            expect(new Cat()).to.instanceOf(Animal);
+        }); 
+});
+   describe("Dog", () => {
+     it("Should be a subclass of Animal", () => {
+       expect(new Dog("Jack", 5, 'Boss', 4, 'dogs')).to.instanceOf(
+         Animal
+       );
+     });
+   });
+
+   describe("Dog", () => {
+     it("Should be a subclass of Animal", () => {
+       expect(new Shark("Nemo", 15,0, 'Shark')).to.instanceOf(
+         Animal
+       );
+     });
+   });
